805ed992dd6545614eb4e0e992657faad2446c25
Raihan Abdillah <raihanabdillahlubis@gmail.com>
2023-11-10 18:12:06 +0700

    Menyelesaikan Bug di Halaman Profil User

--- resources/views/user/components/navbar.blade.php
+++ resources/views/user/components/navbar.blade.php
@@ -73,10 +73,9 @@
                         <i class="fa-solid fa-chevron-right"></i>
                     </a>
 
-                    <form action="/logout" method="POST" class="flex justify-center items-center bg-red-600 text-white">
-                        @csrf
-                        <button class="w-full h-full py-2 px-4">Logout</button>
-                    </form>
+                    <div class="flex justify-center items-center bg-red-600 text-white">
+                        <button onclick="logoutAlert()" type="button" class="w-full h-full py-2 px-4">Logout</button>
+                    </div>
                 </div>
                 {{-- USER DROPDOWN END --}}
 
@@ -84,6 +83,18 @@
         </div>
     </div>
 </nav>
+
+{{-- LOGOUT ALERT START --}}
+<form action="/logout" method="POST" class="w-screen h-full opacity-0 absolute top-0 left-0 backdrop-blur-md z-50 hidden flex justify-center items-center transition duration-300 ease-in-out backdrop-brightness-50" id="logoutAlertPopUp">
+    @csrf
+    <div class="bg-white h-fit w-[30%] rounded-lg shadow-sm shadow-semiBlack py-10 px-8 flex flex-col gap-4 items-center text-center">
+        <i class="text-7xl text-mainColor fa-solid fa-circle-question"></i>
+        <p class="text-2xl font-bold w-[80%]">Apakah Anda yakin ingin keluar dari akun Anda?</p>
+        <button onclick="logoutAlert()" type="button" class="bg-mainColor px-4 w-52 py-2 text-white font-bold rounded-md shadow-md shadow-semiBlack">Kembali</button>
+        <button type="submit" class="bg-mediumRed w-52 px-4 py-2 text-white font-bold rounded-md shadow-md shadow-semiBlack" disabled id="btnLogout">Keluar</button>
+    </div>
+</form>  
+{{-- LOGOUT ALERT END --}}
 
 {{-- agar position fixed navbar tidak diambil content dibawahnya  --}}
 <div class="h-16 w-full"></div>
@@ -118,4 +129,29 @@
             menu.classList.add('opacity-0');
         }
     });
+
+    const logoutAlert = () => {
+        const modal = document.getElementById('logoutAlertPopUp');
+        const button = document.getElementById("btnLogout");
+
+        if (modal.classList.contains('hidden')) {
+            button.disabled = false;
+            requestAnimationFrame(() => {
+                modal.classList.remove('hidden');
+                document.body.classList.add('max-h-[100vh]', 'overflow-hidden');
+                requestAnimationFrame(() => {
+                    modal.classList.add('opacity-100');
+                });
+            });
+        } else {
+            button.disabled = true;
+            requestAnimationFrame(() => {
+                modal.classList.remove('opacity-100');
+                document.body.classList.remove('max-h-[100vh]', 'overflow-hidden');
+                requestAnimationFrame(() => {
+                    modal.classList.add('hidden');
+                });
+            });
+        }
+    }
 </script>
