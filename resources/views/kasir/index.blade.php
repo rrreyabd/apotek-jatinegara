75527326b0c599b89a5d16482a276bff87464976
Raihan Abdillah <raihanabdillahlubis@gmail.com>
2023-11-12 15:55:13 +0700

    Menambahkan Halaman Riwayat Transaksi

--- resources/views/kasir/index.blade.php
+++ resources/views/kasir/index.blade.php
@@ -4,47 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Kasir Apotek | Home</title>
+    <title>Kasir | Home</title>
     @vite('resources/css/app.css')
 
     {{-- FONT AWESOME --}}
     <script src="https://kit.fontawesome.com/e87c4faa10.js" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/1fc4ea1c6a.js" crossorigin="anonymous"></script>
 </head>
-<body class="font-Interflex relative">
+<body class="font-Inter relative">
     
-    {{-- SIDEBAR START --}}
-    <nav class="bg-white p-8 w-[20%] shadow-md absolute left-0 top-0 h-full z-10 shadow-semiBlack -translate-x-80 transition-transform duration-300 ease-in-out flex flex-col gap-4" id="sidebar">
-        <div class="flex justify-between w-full">
-            <p class="bg-mainColor text-white font-bold px-2 py-1 rounded-br-xl rounded-tl-xl text-lg">Jati Negara</p>
-
-            <button onclick="toggleSidebar()" class="text-xl text-black font-bold">
-                &#10005;
-            </button>
-        </div>
-
-        <hr class="border border-1 border-mediumGrey opacity-20">
-
-        <div class="flex flex-col gap-4 py-4 font-bold text-lg text-mediumGrey">
-            <a href="" class="hover:text-mainColor">
-                <i class="mr-4 fa-solid fa-bag-shopping"></i>
-                Produk
-            </a>
-            <a href="" class="hover:text-mainColor">
-                <i class="mr-4 fa-solid fa-cart-shopping"></i>
-                Pesanan Online
-            </a>
-            <a href="" class="hover:text-mainColor">
-                <i class="mr-4 fa-solid fa-clock-rotate-left"></i>
-                Riwayat Transaksi
-            </a>
-            <a href="/logout" class="hover:text-mainColor">
-                <i class="mr-4 fa-solid fa-arrow-right-from-bracket"></i>
-                Logout
-            </a>
-        </div>
-    </nav>
-    {{-- SIDEBAR END --}}
+    @include('kasir.components.sidebar')
+    
     <main class="flex flex-grow bg-plat" id="mainContent">
         <div class="flex flex-col gap-8 w-[67%] p-10">
             <div class="flex gap-8">
