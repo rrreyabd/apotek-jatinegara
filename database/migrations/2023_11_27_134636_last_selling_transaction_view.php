87b8c965b0d4d18969c08e4e6a3fd0b872e1164e
Julyant Anggara <julyantanggara02@gmail.com>
2023-11-28 16:02:19 +0700

    menambahkan procedure add product

--- database/migrations/2023_11_27_134636_last_selling_transaction_view.php
+++ database/migrations/2023_11_27_134636_last_selling_transaction_view.php
@@ -12,13 +12,13 @@ return new class extends Migration
         $sql = "
             CREATE OR REPLACE VIEW last_transaction_view AS
             SELECT 
-                order_complete AS Tanggal_Transaksi, 
+                order_complete AS tanggal_transaksi, 
                 invoice_code, 
-                'Penjualanan' AS Tipe_Transaksi, recipient_bank,
+                'Penjualanan' AS tipe_transaksi, recipient_bank AS metode_pembayaran,
                 (SELECT SUM(Total_Harga(quantity, product_sell_price)) 
                 FROM selling_invoice_details sid 
                 WHERE sid.selling_invoice_id = si.selling_invoice_id
-                GROUP BY sid.selling_invoice_id) AS Total_Pengeluaran
+                GROUP BY sid.selling_invoice_id) AS total_pengeluaran
             FROM 
                 selling_invoices si
             WHERE 
@@ -26,15 +26,15 @@ return new class extends Migration
                 
             UNION
 
-            SELECT order_date AS Tanggal_Transaksi, buying_invoice_id AS invoice_code, 'Pembelian' AS Tipe_Transaksi, 'Tunai' AS recepient_bank,
+            SELECT order_date AS tanggal_transaksi, buying_invoice_id AS invoice_code, 'Pembelian' AS tipe_transaksi, 'Tunai' AS metode_pembayaran,
             (SELECT SUM(Total_Harga(quantity, product_buy_price)) 
                 FROM buying_invoice_details bid 
                 WHERE bid.buying_invoice_id = bi.buying_invoice_id
-                GROUP BY bid.buying_invoice_id) AS Total_Pengeluaran
+                GROUP BY bid.buying_invoice_id) AS total_pengeluaran
             FROM 
                 buying_invoices bi
             ORDER BY
-                Tanggal_Transaksi DESC
+                tanggal_Transaksi DESC
         ";
 
         DB::statement($sql);
---
+++ database/migrations/2023_11_28_082120_add_product_procedure.php
@@ -0,0 +1,75 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        $sql = "
+        DROP PROCEDURE IF EXISTS add_product_procedure;
+
+        CREATE PROCEDURE add_product_procedure(
+            IN product_id CHAR(36),
+            IN description_id CHAR(36),
+            IN product_name VARCHAR(255),
+            IN product_status ENUM('aktif', 'tidak aktif', 'exp'),
+            IN gambar_obat_file VARCHAR(255),
+            IN desc_id CHAR(36),
+            IN kategori CHAR(36),
+            IN golongan CHAR(36),
+            IN satuan_obat CHAR(36),
+            IN NIE VARCHAR(15),
+            IN tipe VARCHAR(255),
+            IN pemasok CHAR(36),
+            IN produksi VARCHAR(255),
+            IN deskripsi LONGTEXT,
+            IN efek_samping LONGTEXT,
+            IN dosis LONGTEXT,
+            IN indikasi LONGTEXT,
+            IN peringatan LONGTEXT,
+            IN harga_beli INT,
+            IN expired_date TIMESTAMP,
+            IN harga_jual INT,
+            IN stock INT,
+            IN detail_id CHAR(36)
+        )
+        BEGIN
+        
+            DECLARE errno INT;
+            DECLARE EXIT HANDLER FOR SQLEXCEPTION
+            BEGIN
+                ROLLBACK;
+            END;
+            
+            START TRANSACTION;
+            
+            INSERT INTO product_descriptions (description_id, category_id, group_id, unit_id, product_DPN, product_type, supplier_id, product_manufacture, product_description, product_sideEffect, product_dosage, product_indication, product_notice, product_photo)
+            VALUES (desc_id, kategori, golongan, satuan_obat, NIE, tipe, pemasok, produksi, deskripsi, efek_samping, dosis, indikasi, peringatan, gambar_obat_file);
+        
+            INSERT INTO products (product_id, product_status, product_name, description_id)
+            VALUES (product_id, product_status, product_name, desc_id);
+        
+            INSERT INTO product_details (product_id, detail_id, product_buy_price, product_expired, product_sell_price, product_stock)
+            VALUES (product_id, detail_id, harga_beli, expired_date, harga_jual, stock);
+            
+            COMMIT;
+            
+        END;
+        ";
+        DB::unprepared($sql);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
